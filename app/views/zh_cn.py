05c6fbe1193d5559ed24894893172be80b13cb34
apoltcuriosity <50551966+apolt-curiosity@users.noreply.github.com>
2020-05-18 23:21:29 +0200

    模板测试

--- app/views/zh_cn.py
+++ app/views/zh_cn.py
@@ -1,10 +1,15 @@
-from flask import Blueprint,render_template
+from flask import Blueprint
+from flask import render_template
+from flask import request
 
 zh_cn = Blueprint('zh_cn', __name__)
 
 @zh_cn.route('/')
 def index():
-    return render_template('zh_cn/home.html')
+    content = {
+        'host_url': request.host_url
+    }
+    return render_template('zh_cn/home.html', **content)
 
 @zh_cn.route('/product/')
 def product():
